734d4dd642ee9eb122b5137e10da6326884ed54d
Taneah Patterson <taneahpatterson@taneahs-mbp.attlocal.net>
2017-07-23 23:41:08 -0400

    updated files

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+// Run the following after the browser loads
+window.onload = function(){
+	// Variables
+	var intervalId;
+	// Timer starts at 120 seconds
+	var timer = 120;
+	// Tracks questions answered correct and incorrect
+	var numCorrect = 0;
+	var numIncorrect= 0;
+
+	// $("start").on("click", decrement);
+	// $("stop").on("click", stop);
+
+	// Post timer to html page
+	$("#timeRemaining").html("<h2>"Time Remaining:  + timer + "</h2>");
+
+	// Once user presses start, activate the timer
+	function start(){
+		intervalId = setInterval(decrement,1000);
+		// console.log(intervalId);
+	}
+	// Run the timer down. Once gets to zero alert "Time is up"
+	function decrement(){
+		timer--;
+		$("#timeRemaining").html("<h2>"Time Remaining:  + timer + "</h2>");
+			timeUp();
+			alert ("Sorry...Time is up!");
+		}
+
+
+	}
+	// When time is up alert player and run the stop function
+	function timeUp(){
+		if (timer===0){
+		alert("Sorry...Time's up!");
+		stop();
+		}
+	}
+}
+ 
+ 	$(".submit").on("click", function(){
+ 		timeUp();   
+ 	 	numCorrect();
+ 	 })
+
+ 	function stop(){
+ 		clearInterval(intervalId);
+ 	}
+
+ 	if ("choices").value==="true"{
+ 		numCorrect+++;
+ 	}
+ 	if ("choices").value==="false"{
+ 		numIncorrect+++;
+ 	}
+ 	
+ 
+ 	
+ 	
